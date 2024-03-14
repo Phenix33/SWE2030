552bc762c1605e478d62ac241a5097f7bdfd42b9
Phenix33 <phenixgikenyi@gmail.com>
2024-03-14 08:20:32 +0300

    coding challenge

---
+++ ifelse/ifelse1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ifelse1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
